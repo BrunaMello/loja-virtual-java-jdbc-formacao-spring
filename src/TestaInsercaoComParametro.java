4f2144252a5bc7f16ca57bf0366e076da735afe8
brunamello <brunacrespomello@gmail.com>
2021-04-01 01:57:10 +0100

    04.Controle de trasação

--- src/TestaInsercaoComParametro.java
+++ src/TestaInsercaoComParametro.java
@@ -4,25 +4,47 @@ public class TestaInsercaoComParametro {
 
     public static void main(String[] args) throws SQLException {
 
-        String nome = "Mouse";
-        String desc = "Testa Mouse";
-
         ConnectionFactory connectionFactory = new ConnectionFactory();
-        Connection con = connectionFactory.recuperarConexao();
 
-        PreparedStatement stm = con.prepareStatement("INSERT INTO PRODUTOS (NOME, DESCRICAO) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);
+        //colocar o connection dentro dos parenteses no try ja faz com que ele feche automaticamente sem precisar explicitar no codigo
+        try (Connection connection = connectionFactory.recuperarConexao()) {
 
+            //retirar o poder de commit do JDBC para o programador controlar a trasacao do inicio ao fim
+            connection.setAutoCommit(false);
+
+            //colocar o prepare statemente dentro dos parenteses no try ja faz com que ele feche automaticamente sem precisar explicitar no codigo
+            try (PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTOS (NOME, DESCRICAO) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS)) {
+
+                adicionarVariavel("SmartTV", "45 polegadas", stm);
+                adicionarVariavel("Radio", " radio bateria", stm);
+
+                //caomitar manualmente
+                connection.commit();
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Roolback executado");
+                //desfazer toda a transacao manualmente
+                connection.rollback();
+            }
+
+        }
+
+    }
+
+    private static void adicionarVariavel(String nome, String desc, PreparedStatement stm) throws SQLException {
         stm.setString(1, nome);
         stm.setString(2, desc);
 
         stm.execute();
 
-        ResultSet rst = stm.getGeneratedKeys();
-        while (rst.next()){
-            Integer id = rst.getInt(1);
-            System.out.println("O ID criado foi: " + id);
-        }
-
+       try (ResultSet rst = stm.getGeneratedKeys()){
+           while (rst.next()){
+               Integer id = rst.getInt(1);
+               System.out.println("O ID criado foi: " + id);
+           }
+       }
 
     }
 }
