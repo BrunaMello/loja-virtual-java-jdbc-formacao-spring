24382b7291a7295714b6b8bb55ee7b7ed7216a67
brunamello <brunacrespomello@gmail.com>
2021-04-01 02:41:57 +0100

    05.Escalabilidade com pool de conexoes

--- src/ConnectionFactory.java
+++ src/ConnectionFactory.java
@@ -1,12 +1,29 @@
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-    public Connection recuperarConexao() throws SQLException {
-        String URL = "jdbc:mysql://localhost:3306/loja_virtual";
+    public DataSource dataSource;
 
-        return DriverManager.getConnection(URL, "root", "#Root123");
+    //configuracao do pool de conexoes
+    public ConnectionFactory(){
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost:3306/loja_virtual");
+        comboPooledDataSource.setUser("root");
+        comboPooledDataSource.setPassword("#Root123");
+
+        //configurar conexoes maximas disponiveis
+        comboPooledDataSource.setMaxPoolSize(15);
+
+
+        this.dataSource = comboPooledDataSource;
+    }
+
+    public Connection recuperarConexao() throws SQLException {
+        //chamando a conexao do construtor acima
+        return this.dataSource.getConnection();
     };
 }
--- src/TestaConexao.java
+++ src/TestaConexao.java
@@ -9,7 +9,6 @@ public class TestaConexao {
 
         con.close();
 
-
     }
 
 }
